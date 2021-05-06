1363c2843b8b1d8cec71893ff777800928b3a164
Sam Dillard <sam@influxdata.com>
2021-05-05 20:12:13 -0700

    add error handling for Config initializer function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     let contents = fs::read_to_string(config.filename).expect("There was a problem opening file");
     let lines = match_lines(&contents, &config.query);
 
@@ -17,10 +21,14 @@ struct Config {
 
 impl Config {
 
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {query, filename}
+        
+        Ok(Config{query, filename})
     }
 }
 
