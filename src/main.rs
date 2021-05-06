dd0beb98ba1d42405e5cdb72558d93beb758e85f
Sam Dillard <sam@influxdata.com>
2021-05-05 19:50:30 -0700

    move parse_args() func to a Config method new()

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_args(&args);
+    let config = Config::new(&args);
     let contents = fs::read_to_string(config.filename).expect("There was a problem opening file");
     let lines = match_lines(&contents, &config.query);
 
@@ -15,10 +15,13 @@ struct Config {
     filename: String
 }
 
-fn parse_args(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {query, filename}
+impl Config {
+
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
 }
 
 fn match_lines(s: &String, q: &String) -> Vec<String> {
